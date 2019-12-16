8693806fbfc59c6f0c29f7a8f5aea698bb275093
Katy Kenny <kgk22@miami.edu>
2019-12-15 20:42:31 -0500

    final

---
+++ ClassExamples/final/contact-form-handler.php
@@ -0,0 +1,26 @@
+<?php
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST ['message'];
+
+    $email_from = 'katykennymusic@gmail.com';
+
+    $email_subject = "New Form Submission";
+
+    $email_body = "User Name: $name.\n".
+                    "User Email: $visitor_email.\n";
+                        "User Message: $message.\n";
+
+    $to = "kgk22@miami.edu";
+
+    $headers = "From: $email_from \r\n";
+
+    $headers .= "Reply-To: $visitor_email \r\n";
+
+    mail($to,$email_subject,$email_body,$headers);
+
+    header("location: final.html")
+
+
+
+ ?>
